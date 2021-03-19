f8385f6eb34a90c158e0d31a157e9c4c0f6ac774
Nate <7nater.nelson@gmail.com>
2021-03-19 14:56:49 -0600

    commit tree b14ee7a155e4a40dfa4d232a047b597c38154a23

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,31 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+  accepts_nested_attributes_for :notes
+
+  def artist_name=(name)
+    artist = Artist.find_or_create_by name: name
+    artist.songs << self
+  end
+
+  def artist_name 
+    self.artist ? self.artist.name : nil
+  end
+
+  def artist_attributes=(artist)
+    self.artist = Artist.find_or_create_by(name: artist[:name])
+    self.artist.update(artist)
+  end
+
+  def genre_attributes=(genre)
+    self.genre = Genre.find_or_create_by(name: genre[:name])
+    self.genre.update(genre)
+  end
+
+  def note_contents=(notes)
+    (notes-[""]).each do |content|
+      self.notes.new.content=content
+    end
+ end
 end
